46eba3cf4932839fe64593e3bcea98db2cc3442d
Sarath Varma Thampuran <svthampuran@gmail.com>
2021-07-01 18:13:27 -0400

    WASD Movement

--- input.js
+++ input.js
@@ -1,16 +1,24 @@
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);
+  stdin.on('data', handleUserInput = (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === '\u0077') {
+      conn.write('Move: up');
+    } else if (key === '\u0061') {
+      conn.write('Move: left');
+    } else if (key === '\u0073') {
+      conn.write('Move: down');
+    } else if (key === '\u0064') {
+      conn.write('Move: right');
+    }
+  });
   return stdin;
-};
-//.....
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
 };
 
 module.exports = {setupInput}
--- play.js
+++ play.js
@@ -2,7 +2,7 @@ const { connect } = require('./client.js')
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
-setupInput();
+let conn = connect();
+setupInput(conn);
 
 
